9b8224fb879f069e3486562ae3e052009f60a5ca
ames123 <126474987+ames123@users.noreply.github.com>
2023-11-13 19:23:10 +0100

    Add files via upload

---
+++ Assets/Scripts/SkryptyMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SkryptyMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Zamykanie()
+    {
+        Application.Quit();
+        Debug.Log("Zamykanie...");
+        GetComponent<AudioSource>().PlayOneShot(GetComponent<AudioSource>().clip);
+    }
+    public void Zaczynamy()
+    {
+        SceneManager.LoadScene("SampleScene"); 
+    }
+}
