131a920738fff2cd80a39946fc58e9a307ced2af
Westesc <mikolaj.kisiel@o2.pl>
2023-11-16 12:35:38 +0100

    switching cameras

---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCamera : MonoBehaviour
+{
+    int indexOfActiveCamera = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Camera>().enabled = false;
+            transform.GetChild(i).GetComponent<AudioListener>().enabled = false;
+        }
+        transform.GetChild(indexOfActiveCamera).GetComponent<Camera>().enabled = true;
+        transform.GetChild(indexOfActiveCamera).GetComponent<AudioListener>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            Debug.Log("zmieniam na kamere ni¿ej");
+            transform.GetChild(indexOfActiveCamera).GetComponent<Camera>().enabled = false;
+            transform.GetChild(indexOfActiveCamera).GetComponent<AudioListener>().enabled = false;
+            indexOfActiveCamera--;
+            if (indexOfActiveCamera < 0)
+                indexOfActiveCamera = transform.childCount-1;
+            transform.GetChild(indexOfActiveCamera).GetComponent<Camera>().enabled = true;
+            transform.GetChild(indexOfActiveCamera).GetComponent<AudioListener>().enabled = true;
+        } else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("zmieniam na kamere wy¿ej");
+            transform.GetChild(indexOfActiveCamera).GetComponent<Camera>().enabled = false;
+            transform.GetChild(indexOfActiveCamera).GetComponent<AudioListener>().enabled = false;
+            indexOfActiveCamera++;
+            indexOfActiveCamera %= transform.childCount;
+            transform.GetChild(indexOfActiveCamera).GetComponent<Camera>().enabled = true;
+            transform.GetChild(indexOfActiveCamera).GetComponent<AudioListener>().enabled = true;
+        }
+    }
+}
